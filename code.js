222d07b0f3618bd98652572b2f9eb3811f6da34b
darmDev <darubiomunoz@gmail.com>
2020-11-03 21:35:43 -0300

    Debugging of the burger menu

--- code.js
+++ code.js
@@ -1,10 +1,8 @@
 const mediaquerie768px = window.matchMedia('screen and (max-width: 768px)');
-const mediaquerie425px = window.matchMedia('screen and (max-width: 425px)');
 const nav = document.querySelector('.nav');
 const burgerButton = document.querySelector('#burger-button');
 
 mediaquerie768px.addListener(validation);
-mediaquerie425px.addListener(validationSecond);
 
 function validation (event) {
     if (event.matches) {
@@ -14,14 +12,9 @@ function validation (event) {
         burgerButton.removeEventListener('click', menu);
     }
 }
-function validationSecond (event) {
-    if (event.matches) {
-            burgerButton.addEventListener('click', menu);
-    }
-    else {
-            burgerButton.removeEventListener('click', menu);
-    }   
-}
+
+validation(mediaquerie768px);
+
 function menu () {
     if (nav.classList.contains('active')) {
         nav.classList.remove('active');
